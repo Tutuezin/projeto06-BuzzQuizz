31923fbcf4991c23284b185d32978b4d661b1995
Kadson <kadson-kaio@hotmail.com>
2022-04-20 20:47:41 -0300

    feat: function returna id quizz ao ser clicado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const API = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes'
 const quizBox = document.querySelector(".boxes-quizz");
 
 let quizzes;
-
+let boxquiz;
 
 function getQuizz(){
     axios.get(API)
@@ -15,11 +15,24 @@ function getQuizz(){
 function writeQuizz(){
     quizBox.innerHTML = ''
     quizzes.forEach(element => {
-        quizBox.innerHTML += `<div class="box-quiz">
-        <img class="gradient" width="340px" height="181px" src="${element.image}" alt="">
+        quizBox.innerHTML += `<div class="box-quiz" id="${element.id}">
+        <img class="gradient"  width="340px" height="181px" src="${element.image}" alt="">
         <span class="title">${element.title}</span>
     </div>`
     });
+    getOneQuiz();
+    
+}
+
+
+function getOneQuiz(){
+    boxquiz = document.querySelectorAll(".box-quiz");
+    boxquiz.forEach(e=>{
+    e.addEventListener('click', function(){
+        console.log(e.id);
+    });
+    
+})
 }
 
 getQuizz();
