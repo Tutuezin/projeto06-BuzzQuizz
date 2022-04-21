531ca86f329a7f58859e0fa04197bb87fb80856f
Arthur Alcantara <vinnycamara1@gmail.com>
2022-04-21 19:42:41 -0300

    js: adiciondo funcoes e parada de deixar aleatorio

--- index.js
+++ index.js
@@ -46,24 +46,13 @@ function verifyId(x) {
     if (x.id == e.id) {
       console.log(e.title);
       quizClickada = e;
+      changeScreen();
       openQuiz();
     }
   });
 }
-getQuizz();
 
-function verifyId(x) {
-  quizzes.forEach((e) => {
-    if (x.id == e.id) {
-      console.log(e.title);
-      quizClickada = e;
-      changeScreen(e);
-    }
-  });
-}
-
-function changeScreen(e) {
-  console.log(e);
+function changeScreen() {
   document.querySelector(".one").classList.toggle("hidden");
   document.querySelector(".two").classList.toggle("hidden");
 }
@@ -73,11 +62,54 @@ function openQuiz() {
   const banner = document.querySelector("#banner");
   const bannerTitle = document.querySelector("#bannerTitle");
 
-  axios.get(`${API}/2`).then((a) => {
-    quizId = a.data;
+  axios.get(`${API}/${quizClickada.id}`).then((e) => {
+    quizId = e.data;
     banner.src = quizId.image;
     bannerTitle.innerHTML = quizId.title;
     console.log(quizId);
   });
+  writeQuestions();
 }
-openQuiz();
+
+function writeQuestions() {
+  topFunction();
+  const questions = document.querySelector(".container");
+
+  for (let i = 0; i < quizClickada.questions.length; i++) {
+    questions.innerHTML += ` <div class="quiz">
+                                <header class="top-quiz" style="background-color: ${quizClickada.questions[i].color}">
+                                    <h4>${quizClickada.questions[i].title}</h4>
+                                </header>
+                             <div class="choices">
+                             </div>
+                             </div>
+                            `;
+    let a = quizClickada.questions[i].answers.sort(comparador);
+    for (let j = 0; j < quizClickada.questions[i].answers.length; j++) {
+      let choice = document.querySelectorAll(".choices");
+      choice[i].innerHTML += `<div class="choice-info quest${i}">
+                                <img src="${a[j].image}" alt="img" width="329px" height="175px">
+                                <p>${a[j].text}</p>
+                            </div>
+                            `;
+    }
+  }
+}
+
+function comparador() {
+  return Math.random() - 0.5;
+}
+
+function reiniciar() {
+  document.querySelector(".container").innerHTML = "";
+  writeQuestions();
+  topFunction();
+}
+
+// Volta pro topo da tela.
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function choiceEvent() {}
